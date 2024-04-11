7fe1a3c9fcd6fd20d5165df7661e249fa15635c1
Benjamin Mosso <benjamin.mosso1701@alumnos.ubiobio.cl>
2024-04-11 09:00:31 -0400

    terminal shell in a box funcionando

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,59 +1,70 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  require 'open3'
 
   def after_sign_in_path_for(resource)
     # Llama a la función para crear/verificar la sesión screen para el usuario
-    create_user_screen_session(resource)
+    create_remote_user(resource)
     super
-
     # Redirige al usuario a su dashboard correspondiente
     if resource.admin?
       admin_dashboard_path
     else
       student_dashboard_path
+  
     end
   end
+  require 'net/ssh'
 
-  def create_user_screen_session(user)
-    Rails.logger.info "Iniciando configuración de usuario para #{user.email}"
+  def create_remote_user(resource)
+    remote_user_name = "#{resource.user_name[0].downcase}#{resource.user_last_name.downcase}"
+    user_home_dir = "/home/#{remote_user_name}"
+    generic_password = "allinf"
+    
+    hostname = 'ec2-54-94-57-100.sa-east-1.compute.amazonaws.com'
+    username = 'shellbox'
+    ssh_password = 'qwerty'
   
-    user_name = "webappuser_#{user.id}"
-    user_password = user.email # Usando el email como contraseña
-    user_group = "webappusers" # Nombre del grupo común para todos los usuarios
-    ssh_host = ENV['SSH_HOST']
-    ssh_user = ENV['SSH_USER']
-    ssh_password = ENV['SSH_PASSWORD']
-    ssh_port = ENV['SSH_PORT'].to_i
+    Net::SSH.start(hostname, username, password: ssh_password) do |ssh|
+      commands = [
+        "echo '#{ssh_password}' | sudo -S groupadd webappusers || true",
+        "echo '#{ssh_password}' | sudo -S useradd -m -d #{user_home_dir} -s /bin/rbash -g webappusers #{remote_user_name} || true",
+        "echo '#{ssh_password}' | sudo -S sh -c \"echo '#{remote_user_name}:#{generic_password}' | chpasswd\""
+      ]
   
-    Net::SSH.start(ssh_host, ssh_user, password: ssh_password, port: ssh_port) do |ssh|
-      # Verifica si el grupo ya existe, si no, créalo
-      unless ssh.exec!("getent group #{user_group}").include?(user_group)
-        ssh.exec!("sudo groupadd #{user_group}")
-        Rails.logger.info "Grupo #{user_group} creado."
-      end
+      commands.each do |command|
+        stdout_data = ""
+        stderr_data = ""
+        exit_code = nil
+        ssh.open_channel do |channel|
+          channel.exec(command) do |ch, success|
+            unless success
+              abort "FAILED: couldn't execute command (ssh.channel.exec)"
+            end
+            channel.on_data do |ch, data|
+              stdout_data += data
+            end
   
-      # Verifica si el usuario ya existe
-      output = ssh.exec!("id #{user_name}")
-      if output.include?("no such user")
-        # Crea el usuario con un shell limitado, sin acceso a sudo y lo agrega al grupo
-        ssh.exec!("sudo useradd -m -s /bin/bash -g #{user_group} #{user_name}")
-        ssh.exec!("echo #{user_name}:#{user_password} | sudo chpasswd")
+            channel.on_extended_data do |ch, type, data|
+              stderr_data += data
+            end
   
-        # Crear directorio de trabajo para el usuario
-        ssh.exec!("sudo mkdir -p /home/#{user_name}/work")
-        ssh.exec!("sudo chown #{user_name}:#{user_group} /home/#{user_name}/work")
-  
-        Rails.logger.info "Usuario #{user_name} creado con éxito en el servidor y agregado al grupo #{user_group}."
-  
-        # Crear sesión screen para el usuario
-        screen_session_name = "session_#{user.id}"
-        ssh.exec!("sudo -u #{user_name} screen -dmS #{screen_session_name}")
-        Rails.logger.info "Sesión screen #{screen_session_name} creada para el usuario #{user_name}."
-      else
-        Rails.logger.info "El usuario #{user_name} ya existe en el servidor."
+            channel.on_request("exit-status") do |ch, data|
+              exit_code = data.read_long
+            end
+          end
+        end
+        ssh.loop
+        
+        puts "stdout: #{stdout_data}"
+        puts "stderr: #{stderr_data}" unless stderr_data.empty?
+        puts "exit code: #{exit_code}"
       end
     end
-  rescue => e
-    Rails.logger.error "Error al crear usuario #{user_name} o al agregarlo al grupo #{user_group}: #{e.message}"
+  rescue Net::SSH::AuthenticationFailed => e
+    puts "Authentication failed for user #{username} on #{hostname}"
   end
+  
+
 end
+
