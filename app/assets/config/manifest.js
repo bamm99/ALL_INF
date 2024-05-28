f5ce08bd3f055df64c90c0794f1ac1248589d6ed
bamm99 <benjamin.mosso1701@alumnos.ubiobio.cl>
2024-05-28 02:44:05 -0400

    home bulma listo

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,4 @@
 //= link_tree ../images
 //= link_tree ../builds
 //= link github_markdown.css
-//= link xterm.css
-
 
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,11 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import { Application } from "@hotwired/stimulus"
-
+import "@hotwired/turbo-rails";
+import "./controllers";
+import "bulma/css/bulma.css";
+import "../assets/stylesheets/application.css";
+import { Application } from "@hotwired/stimulus";
 
 import Rails from "@rails/ujs";
 Rails.start();
 
 require("@rails/activestorage").start();
-
